60d9308f0f6524dc7a56309453ae991373364c14
GLDNRMZ <122560170+GLDNRMZ@users.noreply.github.com>
2024-09-07 08:24:58 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,29 +1,30 @@
 Config = {}
 
-Config.MinimumMethJobPolice = 0
-Config.AlertChance = 90
-Config.Cooldown = 360 --- Cooldown until next allowed meth run
+Config.MinimumMethJobPolice = 0 -- Police Needed
+Config.AlertChance = 100 -- Dispatch Chance
+Config.Cooldown = 360 --- Cooldown
 
-Config.NeedCash = false
-Config.RunCost = 6000
-Config.NeedItem = "usb_green"
+Config.NeedCash = false -- Cash to start?
+Config.RunCost = 6000 -- Amount of cash
+Config.NeedItem = "phone" -- Item to start
+Config.RemoveItem = false --  Remove Item
 
 Config.Payout = math.random(15000, 22000) -- How much you get paid
 Config.Item = "meth" -- The item you receive from the job
-Config.MethChance = 350 -- Percentage chance to get meth
-Config.MethAmount = math.random(5, 10) -- Amount of meth you recieve
+Config.ItemChance = 100 -- Percentage chance to get items
+Config.ItemAmount = math.random(20, 40) -- Amount of items you recieve
 
-Config.SpecialRewardChance = 300 -- Percentage of getting rare item on job. Multiplied by 100. 0.1% = 1, 1% = 10, 20% = 200, 50% = 500, etc. Default 0.1%.
-Config.SpecialItem = "usb_green" -- Put a rare item here which will have 0.1% chance of being given on the run.
+Config.SpecialItemChance = 100 -- Percentage of getting rare item on job.
+Config.SpecialItem = "coke" -- Percentage chance to get item
+Config.SpecialItemAmount = 1
 
 Config.StartModel = "a_m_y_mexthug_01" -- Starting Ped
 Config.StartCoords = vector4(-944.58, 323.74, 70.35, 359.05)
 
-Config.FinishModel = "a_f_y_business_01" -- Starting Ped
+Config.FinishModel = "a_f_y_business_01" -- Finish Ped
 Config.FinishCoords = vector4(-2206.095, 228.346, 173.602, 292.33)
 
-Config.Itemtime = 3 * 1000 -- 5 minutes (time for the case to open after you collect it)
-Config.Carspawn1 = vector3(94.12, -2190.89, 6.0) -- Spawn location for vehicle (it serves not purpose just there...)
+Config.Carspawn1 = vector3(94.12, -2190.89, 6.0) -- Just props
 Config.Carspawn2 = vector3(104.783, -2195.59, 6.038)
 
 Config['methguards'] = {
